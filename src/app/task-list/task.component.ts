141f7bd4212ac0b5d911524ceb979342cf826264
Thuan T. DINH <dinhtienthuan@gmail.com>
2017-01-01 14:14:28 +0700

    1. Update HTML and CSS code for single task. 2. Create Task interface to store task's data: id, name, description, priority, status. 3. Create a hard code of task object to test the UI.

--- src/app/task-list/task.component.ts
+++ src/app/task-list/task.component.ts
@@ -1,157 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import {Task} from "./task";
 
 @Component({
   selector: 'app-task',
   template: `
-    <div class="media media-priority-very-high">
-      <div class="media-left">
-        <i class="fa fa-check fa-fw fa-4x" aria-hidden="true"></i>
-      </div>
+    <div class="media" [ngClass]="task.priority">
+      <div class="media-left">&nbsp;</div>
       <div class="media-body">
-        <h4>Test Task</h4>
-        <p>
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. </p>
+        <h4><i class="fa fa-fw" [ngClass]="task.status" aria-hidden="true"></i> {{ task.name }}</h4>
+        <p *ngIf="task.description">{{ task.description }}</p>
       </div>
     </div>
-    <div class="media media-priority-high">
-      <div class="media-left">
-        <i class="fa fa-ban fa-fw fa-4x" aria-hidden="true"></i>
-      </div>
-      <div class="media-body">
-        <h4>Test Task</h4>
-        <p>
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. </p>
-      </div>
-    </div>
-    <div class="media media-priority-medium">
-      <div class="media-left">
-        <i class="fa fa-fw fa-4x" aria-hidden="true"></i>
-      </div>
-      <div class="media-body">
-        <h4>Test Task</h4>
-        <p>
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. </p>
-      </div>
-    </div>
-    <div class="media media-priority-low">
-      <div class="media-left">
-        <i class="fa fa-check fa-fw fa-4x" aria-hidden="true"></i>
-      </div>
-      <div class="media-body">
-        <h4>Test Task</h4>
-        <p>
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. </p>
-      </div>
-    </div>
-    <div class="media media-priority-very-low">
-      <div class="media-left">
-        <i class="fa fa-check fa-fw fa-4x" aria-hidden="true"></i>
-      </div>
-      <div class="media-body">
-        <h4>Test Task</h4>
-        <p>
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. This is a test task. Do not implement.
-          This is a test task. Do not implement. </p>
-      </div>
-    </div>`,
+  `,
   styles: [`
     .media {
       background-color: #eeeeee;
-    }
-
-    .media-priority-very-high {
-      border: 1px solid #F44336;
-    }
-    
-    .media-priority-very-high .media-left .fa {
-      background: #F44336;
-    }
-
-    .media-priority-high {
-      border: 1px solid #FF9800;
-    }
-    
-    .media-priority-high .media-left .fa {
-      background: #FF9800;
-    }
-
-    .media-priority-medium {
-      border: 1px solid #FFEB3B;
-    }
-    
-    .media-priority-medium .media-left .fa {
-      background: #FFEB3B;
-    }
-
-    .media-priority-low {
-      border: 1px solid #4CAF50;
-    }
-
-    .media-priority-low .media-left .fa {
-      background: #4CAF50;
-    }
-    
-    .media-priority-very-low {
-      border: 1px solid #2196F3;
-    }
-    
-    .media-priority-very-low .media-left .fa {
-      background: #2196F3;
     }
 
     .media-left {
       padding-right: 0px;
     }
     
-    .media-left .fa {
-      height: 1em;
-    }
-
-    .media-left .fa-fw {
-      width: 1em;
-    }
-
     .media-body {
       padding-left: 10px;
+      max-height: 4em;
+      text-overflow: ellipsis;
+    }
+    
+    .media-body .fa-question {
+      color: #0091EA;
+    }
+    
+    .media-body .fa-check {
+      color: #64DD17;
+    }
+    
+    .media-body .fa-ban {
+      color: #D50000;
+    }
+    
+    .media-priority-very-high .media-left {
+      background: #F44336;
+    }
+    
+    .media-priority-high .media-left {
+      background: #FF9800;
+    }
+
+    .media-priority-medium .media-left {
+      background: #FFEB3B;
+    }
+
+    .media-priority-low .media-left {
+      background: #8BC34A;
+    }
+    
+    .media-priority-very-low .media-left {
+      background: #2196F3;
     }
   `]
 })
 export class TaskComponent implements OnInit {
-
+  task:Task = {
+    id: 0,
+    name: 'Test Task',
+    description: 'This is a test task. Do not implement it. It will be deleted after testing.',
+    priority: 'media-priority-very-high',
+    status: 'fa-check',
+  }
   constructor() { }
 
   ngOnInit() {
